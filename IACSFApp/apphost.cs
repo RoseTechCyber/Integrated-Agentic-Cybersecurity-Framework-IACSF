89af49bd92af2f92ba4c92219aa33750da001d35
RoseTechCyber <chinatu7000@outlook.com>
2026-06-14 20:28:28 +0100

    Initial commit with App, Api, and WebApp

---
+++ IACSFApp/apphost.cs
@@ -0,0 +1,7 @@
+#:sdk Aspire.AppHost.Sdk@13.4.3+4f218933552e18ff2874d1b6d5dc3fe671e3b6d9
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+// The aspireify skill will wire up your projects here.
+
+builder.Build().Run();
