5d72dcdd9241505b215a9f1e11f58f53f03836d9
RoseTechCyber <chinatu7000@outlook.com>
2026-07-06 20:43:13 +0100

    Change default CosmosDB key in integration test
    Updated the default CosmosDB key in the test file to a new value.

--- agents/tests/test_cosmos_integration.py
+++ agents/tests/test_cosmos_integration.py
@@ -2,7 +2,7 @@ import os
 from azure.cosmos import CosmosClient
 
 cosmos_uri = os.getenv("COSMOSDB_URI", "https://cosmosdbemulator:8081")
-cosmos_key = os.getenv("COSMOSDB_KEY", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+...==")
+cosmos_key = os.getenv("COSMOSDB_KEY", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;")
 
 client = CosmosClient(cosmos_uri, credential=cosmos_key)
 
