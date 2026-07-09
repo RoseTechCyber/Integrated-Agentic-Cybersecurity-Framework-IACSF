f1fc72459e2db11dd68663e98808e88b1fb4461a
RoseTechCyber <chinatu7000@outlook.com>
2026-07-09 19:04:16 +0100

    Delete IACSFApi/apphost.cs

--- IACSFApi/apphost.cs
+++
@@ -1,7 +0,0 @@
-#:sdk Aspire.AppHost.Sdk@13.4.3+4f218933552e18ff2874d1b6d5dc3fe671e3b6d9
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-// The aspireify skill will wire up your projects here.
-
-builder.Build().Run();
